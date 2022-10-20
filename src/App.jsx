ce302b2c9c14e89675cd5fda36d81d65159b34fd
sazzy-josh <idahosajoshua61@gmail.com>
2022-10-20 13:56:42 +0100

    update: add new events functionality with firebase as baas

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,26 @@ import ViewEvent from './pages/Event'
 import Modal from './components/modal'
 import { useGlobalContext } from './context/stateContext'
 import Navbar from './components/Navbar'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   const { handleSubmit } = useGlobalContext()
   return (
     <>
+     <ToastContainer 
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+     />
      <Navbar/>
       <Routes>  
           <Route path='/' element={<Home />}>
             <Route path='create/new' element={<CreateEvent /> }/>
           </Route> 
+          <Route path='events' element={<>Events</>} />
           <Route path='events/:id' element={<ViewEvent />} >
             <Route path='edit/:id' element={<Modal Value="Done" handleSubmit={handleSubmit}/>}/>
           </Route>
